118e9e17b79fc722c0bbca179d163e9bba619b99
Pierre Boyeau <pierre.boyeau@gmail.com>
2017-03-27 23:54:55 +0200

    Empty functions for bonus part

---
+++ TIVA_TP2a/fourier_interp.m
@@ -0,0 +1,7 @@
+function [ output_args ] = fourier_interp( input_args )
+%FOURIER_INTERP Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ TIVA_TP2a/fourier_zoom.m
@@ -0,0 +1,7 @@
+function [ output_args ] = fourier_zoom( input_args )
+%FOURIER_ZOOM Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
